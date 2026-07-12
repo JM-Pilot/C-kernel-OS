ba6de5e4a3a1f22fa25cad583a40f2ef132a4d8b
JM-Pilot <gabmanlapaz67@gmail.com>
2026-07-12 16:14:17 +0800

    Added License Information in shell 'dlicense' - distribution info 'wlicense' - warranty info added tab support to put_char

--- src/kernel/init/entry.c
+++ src/kernel/init/entry.c
@@ -4,6 +4,7 @@
 #include <string.h> // String tools
 #include <sys/panic.h> // Critical errors
 #include <stdlib.h> // Number to string functions
+#include <stdbool.h>
 #include <drivers/uart/serial.h> // Serial I/O
 #include <drivers/power/power.h> // Power options
 #include <arch/i386/port.h> // Port I/O
@@ -96,6 +97,43 @@ struct cpufreq_s *cpufreq;
 char *no_fb_err = "A framebuffer is required to continue. If you have serial, boot logs will go there.";
 char command[256] = {0};
 
+/* it is in the license where it says
+  If the program does terminal interaction, make it output a short
+  notice like this when it starts in an interactive mode
+ */
+void print_term_license(void)
+{
+	printf("C-Kernel-OS Copyright (C) 2026 orca-pet3910YT\n");
+    	printf("This program comes with ABSOLUTELY NO WARRANTY; for details type 'wlicense'.\n");
+    	printf("This is free software, and you are welcome to redistribute it\n");
+    	printf("under certain conditions; type 'dlicense' for details.\n\n");
+}
+
+void print_term_warranty(void)
+{
+	printf("\tTHERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY\n"
+	"APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT\n"
+	"HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM \"AS IS\" WITHOUT WARRANTY\n"
+	"OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,\n"
+	"THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR\n"
+	"PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM\n"
+	"IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF\n"
+	"ALL NECESSARY SERVICING, REPAIR OR CORRECTION.\n");
+}
+
+void print_term_distrib(void)
+{
+	printf("	This program is free software: you can redistribute it and/or modify\n"
+    	"it under the terms of the GNU General Public License as published by\n"
+    	"the Free Software Foundation, either version 3 of the License, or\n"
+    	"(at your option) any later version.\n"
+    	"This program is distributed in the hope that it will be useful,\n"
+    	"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+    	"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+    	"GNU General Public License for more details.\n"
+    	"You should have received a copy of the GNU General Public License\n"
+    	"along with this program.  If not, see <https://www.gnu.org/licenses/>.\n");
+}
 void kmain(int magic, uint32_t *mbi) {
 	(void)magic;
 	//volatile char* video = (volatile char*)0xB8000;
@@ -271,6 +309,12 @@ void kmain(int magic, uint32_t *mbi) {
 	printk(0, "Welcome to CkOS!");
 	set_post(0x00);
 	//panic("This PC is ass."); // compile with this uncommented to prank people :)
+	printf("Running shell\n");
+
+	/* we would clear this because sometimes the cursor is at the very bottom and it is annoying */
+	clear_screen(); // comment this line if you dont want to clear
+	
+	printf("CkOS Kernel Shell, type 'help' for more info\n");
 	int index = 0;
 	printf("$ ");
 	for (;;) {
@@ -297,6 +341,7 @@ void kmain(int magic, uint32_t *mbi) {
 		flush_term();
 		if (c == '\n') {
 			if (strncmp(command, "help", 4) == 0) {
+				print_term_license();
 				printf("The commands are:\n"
 				"hello: say hello to the world\n"
 				"poweroff: turn the system off (QEMU only)\n"
@@ -421,6 +466,10 @@ void kmain(int magic, uint32_t *mbi) {
 				draw_rect_outline(100, 100, 125, 125, 0x00FFFFFF, 5);
 				delay(3000);
 				clear_screen();
+			} else if (strncmp(command, "wlicense", 9) == 0) {
+				print_term_warranty();
+			} else if (strncmp(command, "dlicense", 9) == 0) {
+				print_term_distrib();
 			} else if (index > 0) {
 				printf("Invalid command: %s\n", command);
 			}
